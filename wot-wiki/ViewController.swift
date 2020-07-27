e65afc25dd3656b97dfccbed841ffd8d4f74d9d4
Dmitiry Shin <dimashin9@gmail.com>
2020-07-27 18:31:02 -0400

    Initial Commit

---
+++ wot-wiki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wot-wiki
+//
+//  Created by Dmitriy Shin on 2020-07-27.
+//  Copyright © 2020 dmitriyshin.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
